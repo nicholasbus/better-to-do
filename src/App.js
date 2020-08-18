8aefea7d82e391e281d26ef016eaa76809598b0d
Nicholas Smith <nicholassmithbus@gmail.com>
2020-08-18 04:48:05 -0600

    did a good chunk of front end logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+  const [itemCount, setItemCount] = useState(0);
+  const [activeItemList, setActiveItemList] = useState([]);
+  const [completedList, setCompletedList] = useState([]);
+  const [show, setShow] = useState(false);
+  const [newInput, setNewInput] = useState('');
+  
+  const [rerender, setRerender] = useState(false)
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleAddNew = () => {
+    let arr = activeItemList;
+    arr.push(newInput)
+    setItemCount(itemCount + 1)
+    setActiveItemList(arr)
+    console.log(activeItemList)
+    handleClose()
+  }
+
+  const handleComplete = (index) => {
+    const clicked = activeItemList[index]
+
+    let newArr = completedList
+    newArr.push(clicked)
+    setCompletedList(newArr)
+
+    let activeArr = activeItemList
+    activeArr.pop(clicked)
+    setActiveItemList(activeArr)
+
+
+    rerender ? setRerender(false) : setRerender(true)
+    
+    console.log(activeItemList)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container fluid id="main-container">
+        <Row>
+          <Col className="main-cols">
+            <h2>status col</h2>
+            <Card>
+              <Card.Header>Stats</Card.Header>
+              <ListGroup variant="flush">
+                <ListGroup.Item>Items Added This Session: </ListGroup.Item>
+                <ListGroup.Item>Items Completed This Session: </ListGroup.Item>
+                <ListGroup.Item>Active Items: </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+          <Col className="main-cols">
+            <h2>Active Items Col</h2>
+            <Button
+              variant="primary"
+              onClick={handleShow}
+              >
+              Add New
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+              
+                <Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Item: </Form.Label>
+                    <Form.Control type="text" placeholder="To-do Item" value={newInput} onChange={(e) => setNewInput(e.target.value)} />
+                  </Form.Group>
+                </Form>
+              
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Cancel
+                </Button>
+                <Button variant="primary" onClick={handleAddNew}>
+                  Add New
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+            {
+              activeItemList.map((item, index) => {
+                return <h4 key={index}>{item}
+                  <Button
+                  variant="success" 
+                  // onClick={handleComplete(index)}
+                  onClick={() => {
+                    handleComplete(index)
+                  
+                  }}
+                  >
+                    Complete
+                  </Button>
+                </h4>
+              })
+            }
+
+
+          </Col>
+          <Col className="main-cols"> 
+            <h2>Completed this session Col</h2>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
